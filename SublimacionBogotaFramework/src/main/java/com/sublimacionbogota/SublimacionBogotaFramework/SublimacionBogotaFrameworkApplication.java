4555cc796395860088a150a309970247b70bc252
Juan <rejuandavid18@gmail.com>
2026-07-21 16:59:52 -0500

    Proyecto inicial con entidad Cliente, repository y controller

---
+++ SublimacionBogotaFramework/src/main/java/com/sublimacionbogota/SublimacionBogotaFramework/SublimacionBogotaFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.sublimacionbogota.SublimacionBogotaFramework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SublimacionBogotaFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SublimacionBogotaFrameworkApplication.class, args);
+	}
+
+}
