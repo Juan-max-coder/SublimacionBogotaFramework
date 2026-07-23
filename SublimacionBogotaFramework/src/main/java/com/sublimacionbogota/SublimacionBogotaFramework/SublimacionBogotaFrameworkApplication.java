51941b34ed590f0bd834c0aaab2f1138991fa59b
Juan <rejuandavid18@gmail.com>
2026-07-23 14:53:56 -0500

    Modificaciones finales del framework

--- SublimacionBogotaFramework/src/main/java/com/sublimacionbogota/SublimacionBogotaFramework/SublimacionBogotaFrameworkApplication.java
+++ SublimacionBogotaFramework/src/main/java/com/sublimacionbogota/SublimacionBogotaFramework/SublimacionBogotaFrameworkApplication.java
@@ -2,12 +2,17 @@ package com.sublimacionbogota.SublimacionBogotaFramework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.sublimacionbogota.framework")
+@EnableJpaRepositories(basePackages = "com.sublimacionbogota.framework.dao")
+@EntityScan(basePackages = "com.sublimacionbogota.framework.modelo")
 public class SublimacionBogotaFrameworkApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SublimacionBogotaFrameworkApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SublimacionBogotaFrameworkApplication.class, args);
+    }
 }
